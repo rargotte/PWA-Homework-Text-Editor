bb8f6db4bc90c8188b0fe5fda6e483ee0e3b4aa2
Regulo Argotte <rargotte@hotmail.com>
2022-04-10 15:47:52 -0500

    First version

---
+++ Develop/client/src/js/register-sw.js
@@ -0,0 +1,13 @@
+// Service Worker registration code
+
+export const registerSW = () => {
+  // Check that service workers are supported
+  if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js');
+    });
+  } else {
+    console.error('Service workers are not supported in this browser.');
+  }
+};
